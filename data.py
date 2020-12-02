f43ac64fa865dfdd2c4b08a8f9ae987b0efc47e0
Tim <tim.kurdov@gmail.com>
2020-12-02 23:32:36 +0300

    v1.0

--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 token1 = '1330349680:AAE7mVKtnL3R1gVZa39-CBK1hg3ob__2fzM'
 token2 = '1342564785:AAG9HqvhQFoSfCctIg2a0MOG8v5QbTjWmaU'
-url = 'https://real-hw-bot.herokuapp.com/.herokuapp.com/'
+url = 'https://real-hw-bot.herokuapp.com/'
 url_temp = 'https://d25c2d913c09.ngrok.io/'
 
 
