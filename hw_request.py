3bbfa399d07037575488e108e7185548359f4699
Tim <tim.kurdov@gmail.com>
2020-12-03 21:32:59 +0300

    add .gitignore and runtime.txt files

--- hw_request.py
+++ hw_request.py
@@ -1,42 +1,36 @@
-import urllib.request
-import urllib.parse
+from urllib3 import PoolManager
 from datetime import datetime, timedelta
 import re
 import json
 
-def get_hw(cur_date=datetime.now().strftime('%Y-%m-%d')):  #cur_date должно быть класса str в формате 'год-месяц-день'
-    cur_date = datetime.strptime(cur_date, '%Y-%m-%d')  #преобразование параметра функции в дату
+def get_hw(cur_date=datetime.now().strftime('%Y-%m-%d')):
+    cur_date = datetime.strptime(cur_date, '%Y-%m-%d')
     
     if cur_date.weekday()==5:
-        cur_date = cur_date+timedelta(days=2)   #если день недели - суббота, дата сдвигается на следующую неделю
+        cur_date = cur_date+timedelta(days=2)
+    
+    with PoolManager() as http:
         
-    r = urllib.request.Request('https://sh-open.ris61edu.ru/auth/login',
-                                data=urllib.parse.urlencode((('login_login', '36_Курдов_Тимофей_36'), ('login_password', 'faYUzEUK'))).encode(),
-                                method='POST',
-                                unverifiable=True)
-    r = urllib.request.urlopen(r)   #залогиниться
-    print(r.code)
-    if r.status>=400:
-        return {'valid': False, 'error': r.geturl()[28:]+'->'+str(r.code)}    #вернуть ошибку в случае неудачи
+        r = http.request('POST', 'https://sh-open.ris61edu.ru/auth/login',
+                         fields={'login_login': '36_Курдов_Тимофей_36', 'login_password': 'faYUzEUK'})
+        print(r.status)
+        if r.status>=400:
+            return {'valid': False, 'error': r.geturl()[28:]+'->'+str(r.status)}
         
-    cookies = [i for i in re.split('[;,] ', r.headers.get('Set-Cookie')) if re.match('(sessionid|NodeID)', i)]   #сохранить cookie-файлы
+        cookies = [i for i in re.split('[;,] ', r.getheader('Set-Cookie')) if re.match('(sessionid|NodeID)', i)]
+        r2 = http.request('GET', 'https://sh-open.ris61edu.ru/personal-area/#diary',
+                          headers={'Cookie':'; '.join(cookies)})
+        print(r2.status)
+        if r2.status>=400:
+            return {'valid': False, 'error': r2.geturl()[28:]+'->'+str(r2.status)}
         
-    r2 = urllib.request.Request('https://sh-open.ris61edu.ru/personal-area/#diary',
-                                headers={'Cookie':'; '.join(cookies)},
-                                method='GET',
-                                unverifiable=True)
-    r2 = urllib.request.urlopen(r2)   #зайти на сайт
-    print(r2.code)
-    if r2.code>=400:
-        return {'valid': False, 'error': r2.geturl()[28:]+'->'+str(r2.code)}    #вернуть ошибку в случае неудачи
-        
-    r3 = urllib.request.Request('https://sh-open.ris61edu.ru/api/ScheduleService/GetDiary',
-                                data=urllib.parse.urlencode((('date', cur_date.strftime('%Y-%m-%d')), ('is_diary', 'true'))).encode(),
-                                headers={'Cookie':'; '.join(cookies)},
-                                method='POST',
-                                unverifiable=True)
-    r3 = urllib.request.urlopen(r3)    #получить расписание от дневника
-    print(r3.code)
-    if r3.code>=400:
-        return {'valid': False, 'error': r3.geturl()[28:]+'->'+str(r3.code)}   #вернуть ошибку в случае неудачи
-    return {'valid': True, 'content': json.loads(r3.read().decode())['days']}
+        r3 = http.request('POST', 'https://sh-open.ris61edu.ru/api/ScheduleService/GetDiary',
+                    fields={'date': cur_date.strftime('%Y-%m-%d'), 'is_diary': 'true'},
+                    headers={'Cookie':'; '.join(cookies),
+                             'Content-Type': 'application/x-www-form-urlencoded',
+                             'Content-Length': '29'},
+                          encode_multipart=False)
+        print(r3.status)
+        if r3.status>=400:
+            return {'valid': False, 'error': r3.geturl()[28:]+'->'+str(r3.status)}
+    return {'valid': True, 'content': json.loads(r3.data.decode())['days']}
