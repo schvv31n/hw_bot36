e806fa779c323fa4c432994e447c885ea22242d4
Tim <tim.kurdov@gmail.com>
2020-12-13 17:31:00 +0300

    several bug fixes

--- data.py
+++ data.py
@@ -1,7 +1,10 @@
-token1 = '1330349680:AAE7mVKtnL3R1gVZa39-CBK1hg3ob__2fzM'
-token2 = '1342564785:AAG9HqvhQFoSfCctIg2a0MOG8v5QbTjWmaU'
-url = 'https://real-hw-bot.herokuapp.com/'
-url_temp = 'https://d25c2d913c09.ngrok.io/'
+TOKEN1 = '1330349680:AAE7mVKtnL3R1gVZa39-CBK1hg3ob__2fzM'
+TOKEN2 = '1342564785:AAG9HqvhQFoSfCctIg2a0MOG8v5QbTjWmaU'
+URL = 'https://real-hw-bot.herokuapp.com/'
+URL_TEMP = 'https://d25c2d913c09.ngrok.io/'
+LOCAL_DB_FILENAME = 'local_hw.json'
+DB_FILENAME = 'hw.json'
+TARGET_CHAT_ID = '-1001265019760'
 
 
-webhook_url = url + token1
+WEBHOOK_URL = URL + TOKEN1
