40a79a8e678fc1aed88f75e0aaaf0c5113457960
Tim <tim.kurdov@gmail.com>
2021-01-13 22:53:18 +0300

    v1.3 beta v1

--- bot.py
+++ bot.py
@@ -5,48 +5,64 @@ import data
 import re
 from hw_request import get_hw
 from pytz import timezone
-import database
 import os
 import json
 import traceback
-updater = tg_ext.Updater(token=data.TOKEN1, use_context=True)
+import sys
+updater = tg_ext.Updater(token=data.TOKEN1, use_context=True)  
 
-HW_SEARCH = re.compile(f"({'|'.join(data.LESSONS_SHORTCUTS)})", re.IGNORECASE)
+HW_SEARCH = re.compile(f"({'|'.join(data.LESSONS_SHORTCUTS)})", re.IGNORECASE)   #простой match обьект для поиска названий предметов
+LESSONS_STARTS = [{'hour': 8, 'minute': 10}, {'hour': 9, 'minute': 0}, {'hour': 9, 'minute': 55}, {'hour': 10, 'minute': 50}, {'hour': 11, 'minute': 45}, {'hour': 12, 'minute': 35}, {'hour': 13, 'minute': 25}]
 
-def errors(update, context):
+#декораторы
+
+def groupadmin_function(f):
+    def decorated(update, context):
+        if update.effective_chat.type!='private':
+            admins = update.effective_chat.get_administrators()
+            if update.message.from_user.id in [i.user.id for i in admins]:
+                f(update, context)
+            else:
+                update.message.reply_text('Данная функция доступна только админам группы')
+        else:
+            update.message.reply_text('Данная функция доступна только в группе')
+    return decorated
+
+def local_hw_cleaner(index):
+    def decorated(context):
+        with open(data.DB_FILENAME) as hw_reader:
+            hw = json.loads(hw_reader.read())
+            
+        today = dt.datetime.now().weekday()
+        if hw['valid']:
+            lessons = hw['content'][today]['lessons']
+            lesson_shortcut = HW_SEARCH.search(lessons[min(len(lessons)-1, index)]['discipline']).groups()[0]
+            if context.dispatcher.chat_data[data.TARGET_CHAT_ID].get(lesson_shortcut):
+                context.dispatcher.chat_data[data.TARGET_CHAT_ID][lesson_shortcut]['outdated'] = True
+    return decorated
+                
+#фоновые функции
+
+def errors(update, context=None):   
     try:
-        raise context.error
+        raise context.error   #структура работы error handler модуля telegram
     except:
-        tb = traceback.format_exc()
-        update.effective_chat.send_message(text='Неизвестная ошибка: \n'+tb)
+        tb = traceback.format_exc()   #преобразовать информацию об ошибке в понятный и подробный вид
+        if type(update)==tg_ext.Updater:    #проверить, передан ли update обьект в функцию
+            update.effective_chat.send_message(text='Неизвестная ошибка: \n'+tb)   #отправить текст ошибки в чат, с которого пошла ошибка
+        else:
+            updater.bot.send_message(chat_id=data.CREATOR_ID, text=tb)  #отправить текст ошибки создателю бота
 updater.dispatcher.add_error_handler(errors)
 
-def stop_bot(update, context):
-    if update.message.from_user.id==420736786:
-        update.message.reply_text('Бот отключен')
-        updater.bot.delete_webhook()
-        updater.stop()
-    else:
-        update.message.reply_sticker(sticker='CAACAgIAAxkBAAMcX6LrsyrpHv72CRJy6kpNife4oIgAAgYAAxLvExmDIS_rRvj5qR4E')
-updater.dispatcher.add_handler(tg_ext.CommandHandler('stop', stop_bot))
+def ny_message(context):
+    print(200)
+    updater.bot.send_message(text='С новым годом!❄', chat_id=data.TARGET_CHAT_ID)
+updater.job_queue.run_once(
+    ny_message,
+    when=dt.datetime(2021, 12, 31, hour=23, minute=30, tzinfo=timezone('Europe/Moscow'))
+)
 
-def exec_script(update, context):
-        if update.message.from_user.id==420736786:
-            updater.dispatcher.run_async(lambda x=None: exec(update.message.text[6:]), update=update)
-updater.dispatcher.add_handler(tg_ext.CommandHandler('exec', exec_script))
-
-updater.job_queue.run_repeating(lambda c: get_hw(), 3600)
-
-def update_hw(update, context):
-    if update.message.from_user.id==420736786:
-        resp = get_hw()
-        if resp['valid']:
-            update.message.reply_text('Д/З обновлено')
-        else:
-            update.message.reply_text('Ошибка: '+resp['error'])
-updater.dispatcher.add_handler(tg_ext.CommandHandler('force_update', update_hw))
-    
-def daily_schedule(update=None, context=None):
+def daily_schedule(context, force=False):
     with open(data.DB_FILENAME) as hw_reader:
         hw = json.loads(hw_reader.read())
         
@@ -54,36 +70,101 @@ def daily_schedule(update=None, context=None):
     if target_weekday==5:
         target_weekday = -1
     target_weekday += 1
-           
+          
     if hw['valid']:
-        parsed_hw = 'Расписание на завтра:\n'
-        photos = []
-        for i in hw['content'][target_weekday]['lessons']:
-            hw_shortcut = HW_SEARCH.search(i['discipline']).groups()[0].lower()
-            db_hw = database.read(hw_shortcut)
-            if type(db_hw)==dict:
-                if db_hw['photoid']:
-                    photos.append((db_hw['photoid'], i['discipline']))
-                lesson_hw = i['homework'] if i['homework'] else db_hw['text']+(f'(фото {len(photos)})' if db_hw['photoid'] else '')
-            else:
-                lesson_hw = i['homework']
-            parsed_hw+=f"{i['discipline']}({i['time_begin'][:5]} - {i['time_end'][:5]})\nД/З - {lesson_hw}\n"
+        if list(hw['content'][target_weekday].keys())[1] not in ['is_weekend', 'is_vacation']:
+            parsed_hw = f"Расписание на {hw['content'][target_weekday]['date']}:\n"
+            photos = []
+        
+            for lesson in hw['content'][target_weekday]['lessons']:
+                shortcut = HW_SEARCH.search(lesson['discipline']).groups()[0].lower()
+                local_hw = updater.dispatcher.chat_data[data.TARGET_CHAT_ID].get(shortcut, None)
+                temp_photo_counter = [len(photos)+1, 0]
+                
+                outdated = False
+                if lesson['homework']:
+                    for material in lesson['materials']:
+                        photos.append({'media': material['url'], 'caption': lesson['discipline']})
+                        temp_photo_counter[1] += 1
+                elif local_hw:
+                    lesson['homework'] = local_hw['text']
+                    outdated = local_hw['outdated']
+                    for link in local_hw['photoid']:
+                        photos.append({'media': link, 'caption': lesson['discipline']})
+                        temp_photo_counter[1] += 1
+                    
+                if temp_photo_counter[1] == 1:
+                    photo_index = f"(фото {temp_photo_counter[0]})"
+                elif temp_photo_counter[1] > 1:
+                    photo_index = f"(фото {temp_photo_counter[0]}-{temp_photo_counter[1]})"
+                else:
+                    photo_index = ''
+                
+                parsed_hw += f"<b>{lesson['discipline']}({lesson['time_begin'][:5]} - {lesson['time_end'][:5]})</b>\nТема: {lesson['theme']}\nД/З: {lesson['homework']+photo_index}{'(устарело!)' if outdated else ''}\n\n"
             
-        sent = updater.bot.send_message(chat_id=data.TARGET_CHAT_ID, text=parsed_hw)
-        if photos:
-            updater.bot.send_media_group(chat_id=data.TARGET_CHAT_ID, media=[tg.InputMediaPhoto(media=i[0], caption=i[1]) for i in photos])
-    else:
-        sent = update.message.reply_text('Не удалось получить расписание на завтра\nОшибка: '+hw['error'])
+            print(parsed_hw)
+            sent = updater.bot.send_message(chat_id=data.TARGET_CHAT_ID, text=parsed_hw, parse_mode='HTML')
+            if photos:
+                updater.bot.send_media_group(chat_id=data.TARGET_CHAT_ID, media=[tg.InputMediaPhoto(**i) for i in photos])
+            pinned = updater.bot.get_chat(data.TARGET_CHAT_ID).pinned_message
+            if pinned.from_user.is_bot:
+                pinned.unpin()
+            sent.pin()
+        else:
+            if force:
+                updater.bot.send_message(chat_id=data.TARGET_CHAT_ID, text='Ошибка: завтра выходной/каникулы')
+updater.job_queue.run_daily(
+    daily_schedule,
+    dt.time(hour=15, tzinfo=timezone('Europe/Moscow')),
+    days=list(range(6))
+)
+
+for i, timestamp in enumerate(LESSONS_STARTS):
+    updater.job_queue.run_daily(
+        local_hw_cleaner(i),
+        dt.time(**timestamp, tzinfo=timezone('Europe/Moscow')),
+        days=list(range(6))
+    )
     
-    updater.bot.get_chat(data.TARGET_CHAT_ID).pinned_message.unpin()
-    sent.pin()
-updater.job_queue.run_daily(daily_schedule, dt.time(hour=15, tzinfo=timezone('Europe/Moscow')), days=list(range(6)))
-updater.dispatcher.add_handler(tg_ext.CommandHandler('debug', daily_schedule))
+updater.job_queue.run_repeating(lambda c: get_hw(), interval=3600, first=0)
+
+#команды создателя
+
+def stop_bot(update, context):
+    if update.message.from_user.id==data.CREATOR_ID:
+        update.message.reply_text('Бот отключен\nЛокально записанное дз: \n'+json.dumps(context.chat_data))
+        updater.bot.delete_webhook()
+        updater.stop()
+        sys.exit()
+updater.dispatcher.add_handler(tg_ext.CommandHandler('stop', stop_bot))
+
+def exec_script(update, context):
+        if update.message.from_user.id==data.CREATOR_ID:
+            updater.dispatcher.run_async(lambda x=None: exec(update.message.text[6:]), update=update)
+updater.dispatcher.add_handler(tg_ext.CommandHandler('exec', exec_script))
+
+#команды даминов
+
+@groupadmin_function
+def update_hw(update, context):
+    sent = update.effective_chat.send_message('Обновляю...')
+    resp = get_hw()
+    if resp['valid']:
+        sent.edit_text('Д/З обновлено')
+    else:
+        sent.edit_text('Ошибка: '+resp['error'])
+updater.dispatcher.add_handler(tg_ext.CommandHandler('force_update', update_hw))
+
+@groupadmin_function    
+def force_schedule(update, context):
+    daily_schedule(context, force=True)
+updater.dispatcher.add_handler(tg_ext.CommandHandler('schedule', force_schedule))
 
 def read_hw(update, context):
     with open(data.DB_FILENAME) as hw_reader:
         res = json.loads(hw_reader.read())
-        
+    
+    groups = context.match.groups()
     target_weekday = dt.datetime.now().weekday()
     end_weekday = None
     if 'на сегодня' not in update.message.text.lower():
@@ -93,51 +174,83 @@ def read_hw(update, context):
     else:
         end_weekday = target_weekday + 1
     
+    #парсинг json-данных с сайта
     hw = None
     if res['valid']:
         for day in res['content'][target_weekday:end_weekday]:
+            if list(day.keys())[1] in ['is_weekend', 'is_vacation']:
+                continue
+                
             for lesson in day['lessons']:
                 groups = context.match.groups()
                 if (groups[2] if groups[2] else groups[3]) in lesson['discipline'].lower():
-                    hw = [lesson['discipline'], day['date'], lesson['homework']]
+                    hw = [lesson['discipline'], day['date'], lesson['homework'], [i['url'] for i in lesson['materials']]]
                     break
             if hw:
                 break
+                
         if not hw:
-            db_hw = database.read((groups[2] if groups[2] else groups[3]))
-            if db_hw:
-                if db_hw['photoid']:
-                    update.message.reply_photo(photo=db_hw['photoid'], caption='Д/З: '+db_hw['text']+'(фото выше)')
-                    return
+            if 'на сегодня' not in update.message.text.lower():
+                db_hw = context.chat_data.get((groups[2] if groups[2] else groups[3]), None)
+                if db_hw:
+                    if db_hw['photoid']:
+                        photos = [tg.InputMediaPhoto(media=db_hw['photoid'][0], caption=f"Д/З: {db_hw['text']}{'(устарело!)' if db_hw['outdated'] else ''}")]
+                        for link in db_hw['photoid'][1:]:
+                            photos.append(tg.InputMediaGroup(media=link))
+                        update.message.reply_media_group(media=photos)
+                    else:
+                        update.message.reply_text('Д/З: '+db_hw['text'])
                 else:
-                    update.message.reply_text('Д/З: '+db_hw['text'])
-                    return
+                    update.message.reply_text('Ошибка: предмет не найден')
             else:
                 update.message.reply_text('Ошибка: предмет не найден')
-                return
+                print(groups[2] if groups[2] else groups[3])
+            return
         elif hw[2]=='':
-            hw[2] = database.read((groups[2] if groups[2] else groups[3]))
+            hw[2] = context.chat_data.get((groups[2] if groups[2] else groups[3]), '')
+            
     else:
-        db_hw = database.read((groups[2] if groups[2] else groups[3]))
-        if db_hw.get('photoid', None)!=None:
-            update.message.reply_photo(photo=db_hw['photoid'], caption='Д/З: '+db_hw['text']+'(фото выше)')
-        elif db_hw.get('text', None)!=None:
-                update.message.reply_text('Д/З: '+db_hw['text'])
+        db_hw = context.chat_data.get((groups[2] if groups[2] else groups[3]), {})
+        if db_hw.get('photoid', None):
+            photos = [tg.InputMediaPhoto(
+                media=db_hw['photoid'][0],
+                caption=f"Д/З: {db_hw['text']}{'(устарело!)' if db_hw['outdated'] else ''}"
+            )]
+            for link in db_hw['photoid'][1:]:
+                photos.append(tg.InputMediaPhoto(media=link))
+            update.message.reply_media_group(media=photos)
+        elif db_hw.get('text', None):
+            update.message.reply_text('Д/З: '+db_hw['text']+('(устарело!)' if db_hw['outdated'] else ''))
         else:
             update.message.reply_text('Ошибка: '+res['error'])
         return
+    #отправка сообщения с данными
     if type(hw[2])==dict:
         if hw[2]['photoid']:
-            update.message.reply_photo(photo=hw[2]['photoid'], caption=f"Д/З по предмету {hw[0]} на {hw[1]}: ")
+            photos = [tg.InputMediaPhoto(media=hw[2]['photoid'][0], caption=f"Д/З по предмету {hw[0]} на {hw[1]}: {hw[2]['text']}{'(устарело!)' if hw[2]['outdated'] else ''}")]
+            for link in hw[2]['photoid'][1:]:
+                photos.append(tg.InputMediaGroup(media=link))
+            update.message.reply_media_group(media=photos)
         else:
             update.message.reply_text(f"Д/З по предмету {hw[0]} на {hw[1]}: {hw[2]['text']}")
     else:
-        update.message.reply_text(f"Д/З по предмету {hw[0]} на {hw[1]}: {hw[2]}")
-p1 = re.compile(f".*((что|че).*по.?({'|'.join(data.LESSONS_SHORTCUTS)})|по.?({'|'.join(data.LESSONS_SHORTCUTS)}).+(что|че)[- ]?(то)?.*зад.*)", re.IGNORECASE)
+        if hw[3]:
+            photos = [tg.InputMediaPhoto(
+                media=hw[3][0],
+                caption=f"Д/З по предмету {hw[0]} на {hw[1]}: {hw[2]}"
+            )]
+            for link in hw[3]:
+                photos.append(tg.InputMediaPhoto(media=link))
+            update.message.reply_media_group(media=photos)
+        else:
+            update.message.reply_text(f"Д/З по предмету {hw[0]} на {hw[1]}: {hw[2]}")
+        
+p1 = re.compile(f"\\b((что|че)\\b.*по.?({'|'.join(data.LESSONS_SHORTCUTS)})|по.?({'|'.join(data.LESSONS_SHORTCUTS)}).+(что|че)[- ]?(то)?.*зад.*)", re.IGNORECASE)
 updater.dispatcher.add_handler(tg_ext.MessageHandler(tg_ext.Filters.regex(p1), read_hw))
 
 p2 = re.compile(f"^({'|'.join(data.LESSONS_SHORTCUTS)}).*[:-] (.*)", re.IGNORECASE)
 def write_hw(update, context):
+    
     if update.message.photo:
         if update.message.caption:
             hw_match = HW_SEARCH.search(update.message.caption)
@@ -148,9 +261,14 @@ def write_hw(update, context):
                 if hw_full_match:
                     actual_hw_text = hw_full_match.groups()[1]
                     
-                database.write(key=hw_match.groups()[0].lower(),
-                               hw_text=actual_hw_text,
-                               hw_photoid=update.message.photo[0].file_id)
+                context.chat_data[hw_match.groups()[0].lower()] = {
+                    'text': actual_hw_text,
+                    'photoid': [i.file_id for i in update.message.photo],
+                    'add_date': dt.datetime.now().strftime('%Y-%m-%d'),
+                    'outdated': False
+                }
+                
+                update.message.reply_text('Д/З записано')
             elif re.compile('не д[ ./]?з[ .]?', re.IGNORECASE).search(update.message.caption):
                 pass
             else:
@@ -158,13 +276,18 @@ def write_hw(update, context):
         else:
             update.message.reply_text('Название какого-либо предмета для записи Д/З не обнаружено, проверьте правильность вашего сообщения; \nЧтобы отметить, что данное фото - не Д/З, подпишите его соответственно') 
     else:
-        database.write(key=HW_SEARCH.search(update.message.text).groups()[0].lower(),
-                       hw_text=context.match.groups()[1])
+        context.chat_data[HW_SEARCH.search(update.message.text).groups()[0].lower()] = {
+            'hw_text': context.match.groups()[1],
+            'photoid': '',
+            'add_date': dt.datetime.now().strftime('%Y-%m-%d'),
+            'outdated': False
+        }
         update.message.reply_text('Д/З записано')
 updater.dispatcher.add_handler(tg_ext.MessageHandler(tg_ext.Filters.regex(p2) | tg_ext.Filters.photo, write_hw))
 
 
 
+updater.bot.send_message(chat_id=data.TARGET_CHAT_ID, text='Бот включен')
 updater.start_webhook(listen='0.0.0.0', port=int(os.environ.get('PORT', 5000)), url_path=data.TOKEN1)
 updater.bot.set_webhook(data.WEBHOOK_URL)
 updater.idle()
---
+++ data.py
@@ -0,0 +1,13 @@
+TOKEN1 = '1330349680:AAE7mVKtnL3R1gVZa39-CBK1hg3ob__2fzM'
+TOKEN2 = '1342564785:AAG9HqvhQFoSfCctIg2a0MOG8v5QbTjWmaU'
+URL = 'https://real-hw-bot.herokuapp.com/'
+URL_TEMP = 'https://d25c2d913c09.ngrok.io/'
+LOCAL_DB_FILENAME = 'local_hw.json'
+DB_FILENAME = 'hw.json'
+TARGET_CHAT_ID = -1001265019760
+LESSONS_SHORTCUTS = ['англ', 'алг', 'био', 'геог', 'физик', 'физ', 'лит', 'хим', 'геом', 'немец', 'фр', 'ист', 'общ', 'рус', 'тех', 'обж', 'родн', 'инф']
+CREATOR_ID = 420736786
+
+
+
+WEBHOOK_URL = URL + TOKEN1
