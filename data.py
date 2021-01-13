93dff45b440e7b7261a0829d9e7047368ea6a0f3
Tim <tim.kurdov@gmail.com>
2021-01-13 23:18:29 +0300

    v1.3 beta v7

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-TOKEN2 = '1342564785:AAG9HqvhQFoSfCctIg2a0MOG8v5QbTjWmaU'
+TOKEN1 = '1330349680:AAE7mVKtnL3R1gVZa39-CBK1hg3ob__2fzM'
 URL = 'https://real-hw-bot.herokuapp.com/'
 DB_FILENAME = 'hw.json'
 TARGET_CHAT_ID = -1001265019760
